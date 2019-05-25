ea13a6a81dfdf11f923f05058e3f8de473087c3c
wusatosi <wusatosi@sina.com>
2019-05-25 16:10:09 +0800

    added authentication client

---
+++ src/main/kotlin/com/kcibald/services/user/AuthenticationClient.kt
@@ -0,0 +1,42 @@
+package com.kcibald.services.user
+
+import io.vertx.core.Vertx
+import io.vertx.core.eventbus.DeliveryOptions
+import io.vertx.core.json.JsonObject
+import io.vertx.core.logging.LoggerFactory
+import io.vertx.kotlin.core.eventbus.sendAwait
+import io.vertx.kotlin.core.json.jsonObjectOf
+import java.util.concurrent.TimeUnit
+
+const val AUTHENTICATION_ENDPOINT = "kcibald.user.authentication"
+
+class AuthenticationClient(
+    private val vertx: Vertx
+) {
+
+    private val logger = LoggerFactory.getLogger(AuthenticationClient::class.java)
+
+    suspend fun verifyLogin(email: String, password: String): Boolean {
+        logger.debug("verifying user $email though user service")
+        val message = vertx
+            .eventBus()
+            .sendAwait<JsonObject>(
+                AUTHENTICATION_ENDPOINT,
+                jsonObjectOf(
+                    "email" to email,
+                    "password" to password
+                ),
+                DeliveryOptions()
+                    .setSendTimeout(TimeUnit.SECONDS.toMillis(2))
+            )
+
+        logger.debug("verify user $email response received, ok")
+        val body = message.body()
+        val validKey = "valid"
+        val value = body.getValue(validKey, null)
+        if (value != null && value is Boolean)
+            return value
+        else
+            throw AssertionError("response should have an \"valid\" of type boolean")
+    }
+}
