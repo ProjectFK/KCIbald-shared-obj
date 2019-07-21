0875d779f28d4722792844357c1e5f79d2bf6c02
wusatosi <wusatosi@sina.com>
2019-07-21 11:55:01 +0800

    fix IDE complain

--- src/main/kotlin/com/kcibald/utils/Immutable.kt
+++ src/main/kotlin/com/kcibald/utils/Immutable.kt
@@ -5,6 +5,6 @@ import java.util.*
 
 // Because JsonObject is just an proxy to its map
 // Making the map unmodifiable will just do the job
-fun JsonObject.immutable(): ImmutableJsonObject = JsonObject(Collections.unmodifiableMap(this.map))
+fun JsonObject.immutable(): ImmutableJsonObject = JsonObject(Collections.unmodifiableMap(this.map) as Map<String, Any>)
 
 typealias ImmutableJsonObject = JsonObject
