ec11b2aabcd98372393f39f2f38ee3d2fdbe7f00
wusatosi <wusatosi@sina.com>
2019-07-25 18:59:20 +0800

    updated AuthenticationClient

--- src/main/kotlin/com/kcibald/services/user/AuthenticationClient.kt
+++ src/main/kotlin/com/kcibald/services/user/AuthenticationClient.kt
@@ -1,12 +1,15 @@
 package com.kcibald.services.user
 
+import com.kcibald.objects.User
+import com.kcibald.services.user.proto.AuthenticationRequest
+import com.kcibald.services.user.proto.AuthenticationResponse
+import com.kcibald.services.user.proto.AuthenticationResponse.Result
 import com.kcibald.utils.d
+import com.kcibald.utils.w
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.DeliveryOptions
-import io.vertx.core.json.JsonObject
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.kotlin.core.eventbus.requestAwait
-import io.vertx.kotlin.core.json.jsonObjectOf
 import java.util.concurrent.TimeUnit
 
 const val AUTHENTICATION_ENDPOINT = "kcibald.user.authentication"
@@ -17,27 +20,81 @@ class AuthenticationClient(
 
     private val logger = LoggerFactory.getLogger(AuthenticationClient::class.java)
 
-    suspend fun verifyCredential(email: String, password: String): Boolean {
+    suspend fun verifyCredential(email: String, password: String): AuthenticationResult {
         logger.d { "verifying user $email though user service" }
-        val message = vertx
-            .eventBus()
-            .requestAwait<JsonObject>(
-                AUTHENTICATION_ENDPOINT,
-                jsonObjectOf(
-                    "email" to email,
-                    "password" to password
-                ),
-                DeliveryOptions()
-                    .setSendTimeout(TimeUnit.SECONDS.toMillis(2))
-            )
+        val message = try {
+            vertx
+                .eventBus()
+                .requestAwait<ByteArray>(
+                    AUTHENTICATION_ENDPOINT,
+                    AuthenticationRequest(email, password).protoMarshal(),
+                    DeliveryOptions()
+                        .setSendTimeout(TimeUnit.SECONDS.toMillis(2))
+                )
+        } catch (e: Exception) {
+            val message = "exception when requesting authentication verification though event bus for user $email"
+            logger.warn(message, e)
+            return AuthenticationResult.SystemError(message)
+        }
 
         logger.d { "verify user $email response received, ok" }
-        val body = message.body()
-        val validKey = "valid"
-        val value = body.getValue(validKey, null)
-        if (value != null && value is Boolean)
-            return value
-        else
-            throw AssertionError("response should have an \"valid\" of type boolean")
+        val body = AuthenticationResponse.protoUnmarshal(message.body())
+        return when (val result = body.result) {
+            is Result.SuccessUser -> {
+                val user = result.successUser
+                AuthenticationResult.Success(
+                    User.createDefault(
+                        user.userId,
+                        user.userName,
+                        user.urlKey,
+                        user.avatarKey,
+                        user.signature
+                    )
+                )
+            }
+            is Result.CommonAuthenticationError -> {
+                when (result.commonAuthenticationError) {
+                    AuthenticationResponse.AuthenticationErrorType.USER_NOT_FOUND
+                    -> AuthenticationResult.UserNotFound
+                    AuthenticationResponse.AuthenticationErrorType.INVALID_CREDENTIAL
+                    -> AuthenticationResult.InvalidCredential
+                    else ->
+                        throw AssertionError("should not have another type of common authentication error")
+                }
+            }
+            is Result.BannedInfo -> {
+                val bannedInfo = result.bannedInfo
+                AuthenticationResult.Banned(
+                    bannedInfo.timeBanned,
+                    bannedInfo.duration,
+                    bannedInfo.message
+                )
+            }
+            is Result.SystemErrorMessage -> {
+                logger.w { "error received from " }
+                AuthenticationResult.SystemError(result.systemErrorMessage)
+            }
+            else ->
+                throw AssertionError("should not have another authentication response type")
+        }
     }
+}
+
+sealed class AuthenticationResult {
+    data class Success(
+        val user: User
+    ) : AuthenticationResult()
+
+    object UserNotFound : AuthenticationResult()
+    object InvalidCredential : AuthenticationResult()
+
+    data class SystemError(
+        val message: String
+    ) : AuthenticationResult()
+
+    data class Banned(
+        val timeBanned: Int,
+        val duration: Int,
+        val message: String
+    ) : AuthenticationResult()
 }
--- src/test/kotlin/com/kcibald/services/user/AuthenticationClientTest.kt
+++ src/test/kotlin/com/kcibald/services/user/AuthenticationClientTest.kt
@@ -1,18 +1,19 @@
 package com.kcibald.services.user
 
+import com.kcibald.objects.User
+import com.kcibald.services.user.proto.AuthenticationRequest
+import com.kcibald.services.user.proto.AuthenticationResponse
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.MessageConsumer
-import io.vertx.core.json.JsonObject
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.core.closeAwait
-import io.vertx.kotlin.core.json.jsonObjectOf
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import java.util.concurrent.TimeUnit
 
@@ -21,26 +22,25 @@ internal class AuthenticationClientTest {
 
     @BeforeEach
     fun setUp(vertx: Vertx) {
-        _messageConsumer = vertx.eventBus().consumer<JsonObject>("kcibald.user.authentication")
+        consumer = vertx.eventBus().consumer("kcibald.user.authentication")
         this._authenticationClient = AuthenticationClient(vertx)
     }
 
-    private var _messageConsumer: MessageConsumer<JsonObject>? = null
+    private lateinit var consumer: MessageConsumer<ByteArray>
     private var _authenticationClient: AuthenticationClient? = null
 
     @Test
     fun verifyLogin_request(context: VertxTestContext) {
         val trueEmail = "example@example.com"
         val truePassword = "42ymRq7FSg"
-        val consumer = _messageConsumer!!
         consumer.handler {
             context.verify {
-                val body = it.body()
-                assertEquals(body.getString("email", "nop"), trueEmail)
-                assertEquals(body.getString("password", "nop"), truePassword)
+                val request = AuthenticationRequest.protoUnmarshal(it.body())
+                assertEquals(request.userEmail, trueEmail)
+                assertEquals(request.plainPassword, truePassword)
                 context.completeNow()
             }
-            it.reply(jsonObjectOf("valid" to true))
+            it.fail(500, "no result for you!")
         }
         runBlocking {
             _authenticationClient!!.verifyCredential(trueEmail, truePassword)
@@ -49,48 +49,145 @@ internal class AuthenticationClientTest {
     }
 
     @Test
-    fun verifyLogin_request_malform() {
-        val trueEmail = "example@example.com"
-        val truePassword = "42ymRq7FSg"
-        val consumer = _messageConsumer!!
+    fun verifyLogin_unexpect_failure() {
         consumer.handler {
-            it.reply(jsonObjectOf("evil key key" to true))
+            it.fail(500, "no result for you!")
         }
-        assertThrows<AssertionError> {
-            runBlocking {
-                _authenticationClient!!.verifyCredential(trueEmail, truePassword)
-            }
-        }
+        val result = fireRequest()
+
+        assert(result is AuthenticationResult.SystemError) { "result should have a type of System Error" }
     }
 
     @Test
-    fun verifyLogin_result_true() {
-        val trueEmail = "example@example.com"
-        val truePassword = "42ymRq7FSg"
-        val consumer = _messageConsumer!!
-        consumer.handler {
-            it.reply(jsonObjectOf("valid" to true))
-        }
-        assertTrue(
-            runBlocking {
-                _authenticationClient!!.verifyCredential(trueEmail, truePassword)
-            }
+    fun verifyLogin_success() {
+        val userId = "user_id"
+        val userName = "user_name"
+        val urlKey = "url_key"
+        val signature = "signature"
+        val avatarKey = "avatarKey"
+
+        val expected = User.createDefault(
+            userId,
+            userName,
+            urlKey,
+            avatarKey,
+            signature
         )
+
+        consumer.handler {
+            val response = AuthenticationResponse(
+                AuthenticationResponse.Result.SuccessUser(
+                    com.kcibald.services.user.proto.User(
+                        userId,
+                        userName,
+                        urlKey,
+                        signature,
+                        avatarKey
+
+                    )
+                )
+            )
+            it.reply(response.protoMarshal())
+        }
+
+        val result = fireRequest()
+        assert(result is AuthenticationResult.Success) { "result should have a type of Success" }
+        result as AuthenticationResult.Success
+
+        assertEquals(expected, result.user)
     }
 
     @Test
-    fun verifyLogin_result_false() {
-        val trueEmail = "example@example.com"
-        val truePassword = "42ymRq7FSg"
-        val consumer = _messageConsumer!!
+    fun verifyLogin_common_failure_incorrectPassword() {
         consumer.handler {
-            it.reply(jsonObjectOf("valid" to false))
+            val response = AuthenticationResponse(
+                AuthenticationResponse.Result.CommonAuthenticationError(
+                    AuthenticationResponse.AuthenticationErrorType.INVALID_CREDENTIAL
+                )
+            )
+            it.reply(response.protoMarshal())
         }
-        assertFalse(
-            runBlocking {
-                _authenticationClient!!.verifyCredential(trueEmail, truePassword)
-            }
+
+        val result = fireRequest()
+
+        assertEquals(AuthenticationResult.InvalidCredential, result)
+    }
+
+
+    @Test
+    fun verifyLogin_common_failure_not_found() {
+        consumer.handler {
+            val response = AuthenticationResponse(
+                AuthenticationResponse.Result.CommonAuthenticationError(
+                    AuthenticationResponse.AuthenticationErrorType.USER_NOT_FOUND
+                )
+            )
+            it.reply(response.protoMarshal())
+        }
+
+        val result = fireRequest()
+
+        assertEquals(AuthenticationResult.UserNotFound, result)
+    }
+
+    @Test
+    fun verifyLogin_sys_fail() {
+        val message = "message"
+        consumer.handler {
+            val response = AuthenticationResponse(
+                AuthenticationResponse.Result.SystemErrorMessage(message)
+            )
+            it.reply(response.protoMarshal())
+        }
+
+        val result = fireRequest()
+
+        assert(result is AuthenticationResult.SystemError) { "result should have a type of SystemError" }
+
+        result as AuthenticationResult.SystemError
+
+        assertEquals(message, result.message)
+    }
+
+    @Test
+    fun verifyLogin_banned() {
+        val timeBanned = 12345
+        val duration = 2124
+        val message = "u suck"
+
+        val expected = AuthenticationResult.Banned(
+            timeBanned,
+            duration,
+            message
         )
+
+        consumer.handler {
+            val response = AuthenticationResponse(
+                AuthenticationResponse.Result.BannedInfo(
+                    AuthenticationResponse.BannedInfo(
+                        timeBanned,
+                        duration,
+                        message
+                    )
+                )
+            )
+            it.reply(response.protoMarshal())
+        }
+
+        val result = fireRequest()
+
+        assert(result is AuthenticationResult.Banned) { "result should have a type of Banned" }
+
+        result as AuthenticationResult.Banned
+
+        assertEquals(expected, result)
+    }
+
+
+    private fun fireRequest(): AuthenticationResult {
+        return runBlocking {
+            _authenticationClient!!.verifyCredential("email", "sureSecurep@ssw0rd!")
+        }
     }
 
     @AfterEach
