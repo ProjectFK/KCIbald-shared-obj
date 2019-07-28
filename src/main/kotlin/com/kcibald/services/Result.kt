e30a9e2cf7119dbf70b2e6ced33a14670a12f9aa
wusatosi <wusatosi@sina.com>
2019-07-28 13:01:27 +0800

    completed Result with test

--- src/main/kotlin/com/kcibald/services/Result.kt
+++ src/main/kotlin/com/kcibald/services/Result.kt
@@ -1,7 +1,19 @@
 package com.kcibald.services
 
 sealed class Result<T> {
-    data class Success<T>(val result: T): Result<T>()
-    object NotFound: Result<Any>()
-    data class Failure<T>(val message: String): Result<T>()
+    data class Success<T> internal constructor(val result: T) : Result<T>()
+    data class Failure<T> internal constructor(val message: String) : Result<T>()
+    class NotFound<T> internal constructor() : Result<T>()
+
+    companion object {
+        private val notFound = NotFound<Any>()
+
+        fun <T> success(result: T): Result<T> = Success(result)
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T> notFound(): Result<T> = notFound as Result<T>
+
+        @Suppress("UNCHECKED_CAST")
+        fun <T> failure(message: String): Result<T> = Failure<T>(message) as Result<T>
+    }
 }
