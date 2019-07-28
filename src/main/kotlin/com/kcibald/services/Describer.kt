6f954b3504773abeb075eb5b86c7d09838dd7d8b
wusatosi <wusatosi@sina.com>
2019-07-28 12:11:39 +0800

    DescribeUserClient and Describer should use suspend functions

--- src/main/kotlin/com/kcibald/services/Describer.kt
+++ src/main/kotlin/com/kcibald/services/Describer.kt
@@ -3,5 +3,5 @@ package com.kcibald.services
 import com.kcibald.interfaces.SubsetIdentifiable
 
 interface Describer<T> {
-    fun describeThoughIdentifier(identifier: SubsetIdentifiable<T>): Result<T>
+    suspend fun describeThoughIdentifier(identifier: SubsetIdentifiable<T>): Result<T>
 }
