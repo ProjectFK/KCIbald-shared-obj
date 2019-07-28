e2993227544d5ff4aa7bfef403e4e134d31cd5ce
wusatosi <wusatosi@sina.com>
2019-07-28 11:41:54 +0800

    DescribeUserClient should implement ServiceClient

--- src/main/kotlin/com/kcibald/services/user/DescribeUserClient.kt
+++ src/main/kotlin/com/kcibald/services/user/DescribeUserClient.kt
@@ -3,7 +3,8 @@ package com.kcibald.services.user
 import com.kcibald.objects.User
 import com.kcibald.services.Describer
 import com.kcibald.services.Result
+import com.kcibald.services.ServiceClient
 
-interface DescribeUserClient: Describer<User> {
+interface DescribeUserClient: Describer<User>, ServiceClient {
     fun describeUser(urlKey: String): Result<User>
 }
