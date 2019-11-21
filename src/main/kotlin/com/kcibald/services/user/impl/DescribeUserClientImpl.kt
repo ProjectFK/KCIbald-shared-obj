b4dc1ceadb1517c123b61529338bd0f27ea1b8d3
wusatosi <26424577+wusatosi@users.noreply.github.com>
2019-11-21 12:51:01 +0800

    Refactored DescribeUserClientImpl

--- src/main/kotlin/com/kcibald/services/user/impl/DescribeUserClientImpl.kt
+++ src/main/kotlin/com/kcibald/services/user/impl/DescribeUserClientImpl.kt
@@ -5,10 +5,12 @@ import com.kcibald.objects.User
 import com.kcibald.services.Result
 import com.kcibald.services.user.DescribeUserClient
 import com.kcibald.services.user.proto.DescribeUserRequest
+import com.kcibald.services.user.proto.DescribeUserRequest.By
 import com.kcibald.services.user.proto.DescribeUserResponse
 import com.kcibald.utils.d
 import com.kcibald.utils.w
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.Message
 import io.vertx.core.eventbus.ReplyException
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.kotlin.core.eventbus.requestAwait
@@ -21,20 +23,48 @@ class DescribeUserClientImpl(
     private val logger = LoggerFactory.getLogger(DescribeUserClientImpl::class.java)
 
     override suspend fun describeUser(urlKey: String): Result<User> =
-        describeUserInternal(DescribeUserRequest.By.UrlKey(urlKey))
+        describeUserInternal(By.UrlKey(urlKey))
 
     override suspend fun describeThoughIdentifier(identifier: SubsetIdentifiable<User>): Result<User> =
-        describeUserInternal(DescribeUserRequest.By.ID(identifier.sid))
+        describeUserInternal(By.ID(identifier.sid))
 
-    private suspend fun describeUserInternal(by: DescribeUserRequest.By): Result<User> {
+    private suspend fun describeUserInternal(by: By): Result<User> {
         logger.d { "sending request to describe user by $by" }
-        val message = try {
-            vertx
-                .eventBus()
-                .requestAwait<ByteArray>(
-                    eventBusAddress,
-                    DescribeUserRequest(by).protoMarshal()
-                )
+        val message = performRequestNullIfFail(by)
+
+        if (message == null)
+            return Result.failure("exception when performing request")
+
+        return parseAndProcessMessage(message)
+    }
+
+    private fun parseAndProcessMessage(message: Message<ByteArray>): Result<User> {
+        return when (val result = parseResultFromMessage(message)) {
+            is DescribeUserResponse.Result.UserNotFound ->
+                Result.notFound()
+            is DescribeUserResponse.Result.SystemErrorMessage ->
+                Result.failure(result.systemErrorMessage)
+            is DescribeUserResponse.Result.SingleUserResult ->
+                Result.success(translateDBUser(result.singleUserResult.result!!))
+            else ->
+                throw AssertionError("should not reach here")
+        }
+    }
+
+    private fun translateDBUser(dbUser: com.kcibald.services.user.proto.User): User =
+        User.createDefault(
+            dbUser.userName,
+            dbUser.urlKey,
+            dbUser.avatarKey,
+            dbUser.signature
+        )
+
+    private fun parseResultFromMessage(message: Message<ByteArray>) =
+        DescribeUserResponse.protoUnmarshal(message.body()).result
+
+    private suspend fun performRequestNullIfFail(by: By): Message<ByteArray>? {
+        try {
+            return sendRequest(by)
         } catch (e: ReplyException) {
             logger.w {
                 "reply exception when trying to send request to describe user by $by, e: $e"
@@ -45,29 +75,17 @@ class DescribeUserClientImpl(
                 "unexpected exception when performing request though event bus ($eventBusAddress), " +
                         "attempting to describe user by $by, exception received: $e"
             }
-            return Result.failure("exception when performing request")
-        }
-
-        return when (val result = DescribeUserResponse.protoUnmarshal(message.body()).result) {
-            is DescribeUserResponse.Result.UserNotFound ->
-                Result.notFound()
-            is DescribeUserResponse.Result.SystemErrorMessage ->
-                Result.failure(result.systemErrorMessage)
-            is DescribeUserResponse.Result.SingleUserResult -> {
-                val dbUser = result.singleUserResult.result!!
-                val user = User.createDefault(
-                    dbUser.userName,
-                    dbUser.urlKey,
-                    dbUser.avatarKey,
-                    dbUser.signature
-                )
-                Result.success(user)
-            }
-//            There's no muti-result yet
-            else ->
-                throw AssertionError("should not reach here")
+            return null
         }
     }
+
+    private suspend fun sendRequest(by: By): Message<ByteArray> =
+        vertx
+            .eventBus()
+            .requestAwait(
+                eventBusAddress,
+                DescribeUserRequest(by).protoMarshal()
+            )
 
     override val clientVersion: String = "PREVIEW"
 
